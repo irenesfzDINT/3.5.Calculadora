e969eebd538b61d762781e2d133727b9955d2f32
irene <irene.sanchez@iesdoctorbalmis.com>
2021-10-07 14:36:26 +0200

    ejercicio terminado

--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PantallaTextBlock.Text += (sender as Button).Tag.ToString();
+        }
     }
 }
